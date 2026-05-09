02cbe864ffbe6a484b28a7af8ad7f5de3da28efd
Federico Gravina <gravinadavilafederico@gmail.com>
2026-05-09 20:49:00 -0300

    fix: replace deprecated ColorCorrection with ToneMapping + color grading stack

--- src/components/PostFX.tsx
+++ src/components/PostFX.tsx
@@ -1,13 +1,51 @@
 import React from "react";
-import { EffectComposer, Bloom, SSAO, Vignette, ColorCorrection } from "@react-three/postprocessing";
+import { EffectComposer, Bloom, SSAO, Vignette, BrightnessContrast, HueSaturation, ToneMapping } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 
 export default function PostFX() {
   return (
     <EffectComposer multisampling={0}>
-      <Bloom intensity={0.45} luminanceThreshold={0.24} luminanceSmoothing={0.16} />
-      <SSAO samples={16} radius={0.32} intensity={26} luminanceInfluence={0.16} bias={0.04} />
-      <Vignette eskil={false} offset={0.16} darkness={0.92} />
-      <ColorCorrection powRGB={[1.02, 1.0, 0.98]} mulRGB={[1.07, 0.98, 0.92]} />
+      <ToneMapping
+        blendFunction={BlendFunction.NORMAL}
+        adaptive={true}
+        resolution={256}
+        middleGrey={0.6}
+        maxLuminance={16}
+        averageLuminance={1}
+        adaptationRate={1}
+      />
+
+      <Bloom
+        intensity={0.38}
+        luminanceThreshold={0.22}
+        luminanceSmoothing={0.18}
+      />
+
+      <SSAO
+        samples={16}
+        rings={4}
+        distanceThreshold={0.15}
+        distanceFalloff={0.25}
+        rangeThreshold={0.5}
+        intensity={24}
+        bias={0.04}
+      />
+
+      <BrightnessContrast
+        brightness={0.02}
+        contrast={0.08}
+      />
+
+      <HueSaturation
+        hue={-0.02}
+        saturation={0.06}
+      />
+
+      <Vignette
+        eskil={false}
+        offset={0.15}
+        darkness={0.92}
+      />
     </EffectComposer>
   );
 }
